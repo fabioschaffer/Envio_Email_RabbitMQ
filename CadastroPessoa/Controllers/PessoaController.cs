3b8a8890e4a96ddd8716b1baaf8897db17bc0938
Fabio Schaffer <fabioschaffer@gmail.com>
2026-05-01 19:22:31 -0300

    Add project files.

---
+++ CadastroPessoa/Controllers/PessoaController.cs
@@ -0,0 +1,26 @@
+using CadastroPessoa.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroPessoa.Controllers;
+
+[ApiController]
+[Route("pessoa")]
+public class PessoaController : ControllerBase {
+
+    [HttpPost("criar")]
+    public ActionResult Criar([FromBody] Pessoa pessoa) {
+        if (pessoa == null) return BadRequest("Dados inválidos");
+
+        var rabbit = new CriaMensagemRabbitMQ();
+
+        rabbit.CriaMensagem();
+
+        return Ok(pessoa);
+    }
+
+}
+
+public class Pessoa {
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+}
