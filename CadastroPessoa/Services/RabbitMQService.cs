f1a8dbe0b315de58c7a1e10888850f4d3dc312dc
Fabio Schaffer <fabioschaffer@gmail.com>
2026-05-02 12:42:00 -0300

    ajustes na fila de envio.

---
+++ CadastroPessoa/Services/RabbitMQService.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace CadastroPessoa.Services;
+
+public class CriaMensagemRabbitMQ {
+
+    const string QUEUE_NAME = "fila_1";
+
+    public async Task CriaMensagem() {
+
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queue: QUEUE_NAME, durable: true, exclusive: false, autoDelete: false,
+            arguments: new Dictionary<string, object?> { { "x-queue-type", "quorum" } });
+
+        const string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(exchange: string.Empty, routingKey: QUEUE_NAME, body: body);
+
+        Console.WriteLine($"Sent {message}");
+    }
+}
